bb3396a1922fc819d25973e2daff9f219ce662a3
pavanikoyyana <85073663+pavanikoyyana@users.noreply.github.com>
2021-08-04 13:23:50 +0530

    Create problem 1.c

---
+++ DSA/Recursion/problem 1.c
@@ -0,0 +1,18 @@
+ // program to write n natural numbers in increasing order //
+
+
+#include <stdio.h>
+ int fun(int n)
+{   if (n==0)
+     return;
+    fun(n-1);
+    printf("%d ",n);}
+    int main()
+{
+   int n;
+   fun(5);
+    return 0;
+}
+
+
+output : 1 2 3 4 5
